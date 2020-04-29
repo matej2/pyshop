b18f1238fc7fca6c2a783dafca694f15ba051668
Matej2 <11059438+matej2@users.noreply.github.com>
2020-04-29 18:35:31 +0200

    User view code refactoring

--- users/views.py
+++ users/views.py
@@ -17,9 +17,9 @@ def register(request):
             #form.save()
             username = form.cleaned_data.get('username')
             messages.success(request, f'Account created for {username}!')
-            return redirect(views.index)
+            return redirect("home")
         else:
-            messages.success(request, f'Not valid, please check form!')
+            messages.error(request, 'Not valid, please check form!')
     else:
         form = UserCreationForm()
     return render(request, 'register.html', {'form': form})
